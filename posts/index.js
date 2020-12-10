9b05f1e548cfd3fbe947c8830ac9306231c1863c
makeze <max.gana93@gmail.com>
2020-12-10 23:01:35 +0000

    microservice post added

--- posts/index.js
+++ posts/index.js
@@ -1,14 +1,23 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const {randomBytes} = require('crypto');
 
 const app = express();
+app.use(bodyParser);
 
+const posts = {};
 
 app.get('/posts', (req, res) => {
-
+    res.send(posts);
 });
 
 app.post('/posts', (req, res) => {
-
+    const id = randomBytes(4).toString('hex');
+    const {title} = req.body;
+    posts[id] = {
+        id, title
+    };
+    res.send(201);
 });
 
 app.listen(4000, () => {
