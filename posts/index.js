ec63a43eb8a784fc52a95211e0e3890670950d76
makeze <max.gana93@gmail.com>
2020-12-11 21:24:25 +0000

    react app for microservices

--- posts/index.js
+++ posts/index.js
@@ -1,25 +1,30 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const {randomBytes} = require('crypto');
+const { randomBytes } = require('crypto');
+const cors = require('cors');
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 const posts = {};
 
 app.get('/posts', (req, res) => {
-    res.send(posts);
+  res.send(posts);
 });
 
 app.post('/posts', (req, res) => {
-    const id = randomBytes(4).toString('hex');
-    const {title} = req.body;
-    posts[id] = {
-        id, title
-    };
-    res.send(201).send(posts[id]);
+  const id = randomBytes(4).toString('hex');
+  const { title } = req.body;
+
+  posts[id] = {
+    id,
+    title
+  };
+
+  res.status(201).send(posts[id]);
 });
 
-app.listen(4100, () => {
-    console.log('listening on 4100');
+app.listen(4000, () => {
+  console.log('Listening on 4000');
 });
