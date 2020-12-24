c5175badf6696891b22d6ef637271860615e977a
makeze <max.gana93@gmail.com>
2020-12-24 16:59:46 +0100

    moderation service added

--- moderation/index.js
+++ moderation/index.js
@@ -5,8 +5,23 @@ const axios = require('axios');
 const app = express();
 app.use(bodyParser.json());
 
-app.post('/events', (req, res) => {
+app.post('/events', async (req, res) => {
+    const {type, data} = req.body;
 
+    if (type === 'CommentCreated') {
+        const status = data.content.includes('red') ? 'rejected' : 'approved';
+
+        await axios.post('http://localhost:4005/events', {
+            type: 'CommentModerated',
+            data: {
+                id: data.id,
+                postId: data.postId,
+                content: data.content,
+                status
+            }
+        });
+    }
+    res.send({});
 });
 
 app.listen(4003, () => {
