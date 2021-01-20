f822bf49bc1dc4ebc0e63b4405fe75fb035c7098
makeze <max.gana93@gmail.com>
2021-01-21 00:47:50 +0100

    added cookie session

--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -2,16 +2,25 @@ import express from 'express';
 import 'express-async-errors';
 import {json} from 'body-parser';
 import mongoose from 'mongoose';
+import cookieSession from "cookie-session";
 
-import {currentUserRouter} from "./currentuser";
-import {signInRouter} from "./signin";
-import {signOutRouter} from "./signout";
-import {signUpRouter} from "./signup";
-import {errorHandler} from "../middlewares/error-handler";
-import {NotFoundError} from "../errors/not-found-error";
+import {currentUserRouter} from "./routes/currentuser";
+import {signInRouter} from "./routes/signin";
+import {signOutRouter} from "./routes/signout";
+import {signUpRouter} from "./routes/signup";
+import {errorHandler} from "./middlewares/error-handler";
+import {NotFoundError} from "./errors/not-found-error";
+
 
 const app = express();
+app.set('trust proxy', true);
 app.use(json());
+app.use(
+    cookieSession({
+       signed: false,
+       secure: true
+    })
+);
 
 app.use(currentUserRouter);
 app.use(signInRouter);
