eddef6189c02561096e7e73dcfba62b05e4b3112
makeze <max.gana93@gmail.com>
2021-01-23 17:01:22 +0100

    jwt token support added

--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -34,6 +34,10 @@ app.get('*', async (req, res) => {
 app.use(errorHandler);
 
 const start = async () => {
+    if(!process.env.JWT_KEY) {
+        throw new Error('JWT KEY is not defined');
+    }
+
     try{
         await mongoose.connect('mongodb://auth-mongo-srv:27017/auth',{
             useNewUrlParser: true,
--- ticketing/auth/src/models/user.ts
+++ ticketing/auth/src/models/user.ts
@@ -18,13 +18,23 @@ interface UserDoc extends mongoose.Document {
 }
 
 const userSchema = new mongoose.Schema({
-    email: {
-        type: String,
-        required: true
+        email: {
+            type: String,
+            required: true
+        },
+        password: {
+            type: String,
+            required: true
+        }
     },
-    password: {
-        type: String,
-        required: true
+    {
+        toJSON: {
+            transform(doc, ret) {
+                ret.id = ret._id;
+                delete ret._id;
+                delete ret.password;
+                delete ret.__v;
+        }
     }
 });
 
