939483532797779b83b82c4a048b64c07b04b914
makeze <max.gana93@gmail.com>
2021-02-28 23:04:46 +0100

    cookie for data fetch on ssr

--- ticketing/client/pages/index.js
+++ ticketing/client/pages/index.js
@@ -1,3 +1,30 @@
-export default () => {
-    return <h1>Landing</h1>
+import axios from 'axios';
+
+const LandingPage = ({currentUser}) => {
+    console.log(currentUser);
+    return <h1>Landing</h1>;
 }
+
+LandingPage.getInitialProps = async ({req}) => {
+    console.log(req.headers);
+    if (typeof window === 'undefined') {
+        // server
+        const {data} = await axios.get(
+            'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
+            {
+                headers: {
+                    Host: 'maksudik.dev'
+                }
+            }
+        )
+        return data;
+    } else {
+        // browser
+        const {data} = await axios.get('/api/users/currentuser');
+        console.log('I am on the server!');
+        return data;
+    }
+    return {};
+};
+
+export default LandingPage;
