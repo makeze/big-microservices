e4e590c2c82bd15f5ba1200dffa9c3afb5f49eef
bisquitt <max.gana93@gmail.com>
2021-05-18 02:09:38 +0200

    async publish method

--- ticketing/nats-test/src/publisher.ts
+++ ticketing/nats-test/src/publisher.ts
@@ -7,22 +7,17 @@ const stan = nats.connect('ticketing', 'abc', {
     url: 'http://localhost:4222'
 });
 
-stan.on('connect', () => {
-   console.log('Publisher connected to NATS');
+stan.on('connect', async () => {
+    console.log('Publisher connected to NATS');
 
-   const publisher = new TicketCreatedPublisher(stan);
-    publisher.publish({
-        id: '123',
-        title: 'music',
-        price: 39.99
-    })
-   /*const data = JSON.stringify({
-       id: 1,
-       title: 'title',
-       price: 5
-   });
-
-   stan.publish('ticket:created', data, () => {
-       console.log('Event published');
-   })*/
+    const publisher = new TicketCreatedPublisher(stan);
+    try {
+        await publisher.publish({
+            id: '123',
+            title: 'music',
+            price: 39.99
+        })
+    } catch (err) {
+        console.error(err);
+    }
 });
