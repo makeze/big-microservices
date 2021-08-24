f1505387317c92e7b338d4a7cdee427323d79de2
Maksik <max.gana93@gmail.com>
2021-08-24 14:39:01 +0300

    Updates

--- ticketing/common/src/events/ticket-created-event.ts
+++ ticketing/common/src/events/ticket-created-event.ts
@@ -4,7 +4,6 @@ export interface TicketCreatedEvent {
   subject: Subjects.TicketCreated;
   data: {
     id: string;
-    version: number;
     title: string;
     price: number;
     userId: string;
