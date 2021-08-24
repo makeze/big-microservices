5ed4e0ed86ea42effbb8c32e17eb06798593b837
Maksik <max.gana93@gmail.com>
2021-08-24 14:18:29 +0300

    nats singleton

--- ticketing/tickets/src/index.ts
+++ ticketing/tickets/src/index.ts
@@ -1,6 +1,7 @@
 import mongoose from 'mongoose';
 
 import { app } from './app';
+import {natsWrapper} from './nats-wrapper';
 
 const start = async () => {
   if (!process.env.JWT_KEY) {
@@ -11,6 +12,7 @@ const start = async () => {
   }
 
   try {
+    await natsWrapper.connect('ticketing', 'clientId', 'http://nats-srv:4222');
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
