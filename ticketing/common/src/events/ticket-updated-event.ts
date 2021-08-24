e3cd84c39972883a8bc3df6cd1f4737286163a4e
Maksik <max.gana93@gmail.com>
2021-08-24 15:16:45 +0300

    Updates

--- ticketing/common/src/events/ticket-updated-event.ts
+++ ticketing/common/src/events/ticket-updated-event.ts
@@ -4,10 +4,8 @@ export interface TicketUpdatedEvent {
   subject: Subjects.TicketUpdated;
   data: {
     id: string;
-    version: number;
     title: string;
     price: number;
     userId: string;
-    orderId?: string;
   };
 }
