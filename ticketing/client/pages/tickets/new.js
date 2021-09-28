a13482f80477651afde8f5eba9fc5b94f920d230
makeze <max.gana93@gmail.com>
2021-09-28 23:57:56 +0200

    client ticket create form

--- ticketing/client/pages/tickets/new.js
+++ ticketing/client/pages/tickets/new.js
@@ -1,62 +1,33 @@
-import { useState } from 'react';
-import Router from 'next/router';
-import useRequest from '../../hooks/use-request';
+import {useState} from 'react';
 
 const NewTicket = () => {
-  const [title, setTitle] = useState('');
-  const [price, setPrice] = useState('');
-  const { doRequest, errors } = useRequest({
-    url: '/api/tickets',
-    method: 'post',
-    body: {
-      title,
-      price,
-    },
-    onSuccess: () => Router.push('/'),
-  });
+    const [title, setTitle] = useState('');
+    const [price, setPrice] = useState('');
 
-  const onSubmit = (event) => {
-    event.preventDefault();
+    const onBlur = () => {
+        const value = parseFloat(price);
 
-    doRequest();
-  };
+        if (isNaN(value)) {
+            return;
+        }
 
-  const onBlur = () => {
-    const value = parseFloat(price);
-
-    if (isNaN(value)) {
-      return;
+        setPrice(value.toFixed(2));
     }
 
-    setPrice(value.toFixed(2));
-  };
-
-  return (
-    <div>
-      <h1>Create a Ticket</h1>
-      <form onSubmit={onSubmit}>
-        <div className="form-group">
-          <label>Title</label>
-          <input
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-            className="form-control"
-          />
-        </div>
-        <div className="form-group">
-          <label>Price</label>
-          <input
-            value={price}
-            onBlur={onBlur}
-            onChange={(e) => setPrice(e.target.value)}
-            className="form-control"
-          />
-        </div>
-        {errors}
-        <button className="btn btn-primary">Submit</button>
-      </form>
+    return <div>
+        <h1>Create a ticket</h1>
+        <form>
+            <div className="form-group">
+                <label>Title</label>
+                <input value={title} onChange={(e) => setTitle(e.target.value)} className="form-control"/>
+            </div>
+            <div className="form-group">
+                <label>Price</label>
+                <input value={price} onBlur={onBlur} onChange={(e) => setPrice(e.target.price)} className="form-control"/>
+            </div>
+            <button className="btn btn-primary">Submit</button>
+        </form>
     </div>
-  );
-};
+}
 
 export default NewTicket;
